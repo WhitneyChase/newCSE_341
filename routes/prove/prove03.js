4a6fd2f0d3fa6eb1b568e53e58f5ef50a62057c1
WhitneyChase <64234289+WhitneyChase@users.noreply.github.com>
2020-10-03 17:18:04 -0600

    Create prove03.js

---
+++ routes/prove/prove03.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const proveAssignments = require('../controllers/prove');
+const router = express.Router();
+
+router.get('/', proveAssignments.getProve03);
+
+
+router.post('/submit', proveAssignments.postProve03);
+
+module.exports = router;
