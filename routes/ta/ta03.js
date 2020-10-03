cd30799f0289cae3deee7281e13c50e39995f376
WhitneyChase <64234289+WhitneyChase@users.noreply.github.com>
2020-10-03 17:19:39 -0600

    Update and rename routes/ta03.js to routes/ta/ta03.js

---
+++ routes/ta/ta03.js
@@ -0,0 +1,12 @@
+//TA03 PLACEHOLDER
+const express = require('express');
+const router = express.Router();
+const TeamActivities = require('../controllers/ta');
+
+
+//get the the info from the add user form
+router.post('/ta03', TeamActivities.postTeam03);
+
+router.get('/', TeamActivities.getTeam03);
+
+module.exports = router;
