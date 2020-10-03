b103061bef020100883f45d83fc7c5da58c9f01b
WhitneyChase <64234289+WhitneyChase@users.noreply.github.com>
2020-10-03 17:17:30 -0600

    Update and rename routes/prove02 to routes/prove/prove02.js

---
+++ routes/prove/prove02.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const proveAssignments = require('../controllers/prove');
+const router = express.Router();
+
+router.get('/', proveAssignments.getProve02);
+
+
+router.post('/submit', proveAssignments.postProve02);
+
+module.exports = router;
