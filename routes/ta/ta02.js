630895fcac0d9e2eb23b87b4241c2027435f7bae
WhitneyChase <64234289+WhitneyChase@users.noreply.github.com>
2020-10-03 17:19:10 -0600

    Update and rename routes/ta02.js to routes/ta/ta02.js

---
+++ routes/ta/ta02.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const TeamActivities = require('../controllers/ta');
+const router = express.Router();
+
+
+
+//get the the info from the add user form
+router.post('/addUser', TeamActivities.postTeam02AddUser);
+
+
+//get the the info from the remove user form
+router.post('/removeUser', TeamActivities.postTeam02RemoveUser);
+
+
+router.get('/', TeamActivities.getTeam02);
+
+module.exports = router;
