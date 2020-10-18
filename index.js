7a4959b40564ec1f954a2bf288d1ed980eb72fbb
WhitneyChase <64234289+WhitneyChase@users.noreply.github.com>
2020-10-17 22:56:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,28 +5,79 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const routes = require('./routes');
 const PORT = process.env.PORT || 5000 // So we can run on heroku || (OR) localhost:5000
+const flash = require('connect-flash');
 const app = express();
 const mongoConnect = require('./utils/database');
 const User = require('./models/user');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
 
+
+
+const errorController = require('./controllers/shop/error')
+// const User = require('./models/user');
+
+
+const MONGODB_URI =
+  'mongodb+srv://Whitney-Chase-01:nitney15020@cluster01.clet0.mongodb.net/products?retryWrites=true&w=majority';
+
+  // const app = express();
+  const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+  });
+  const csrfProtection = csrf();
+  
+  app.set('view engine', 'ejs');
+  app.set('views', 'views');
 
 app.use(express.static(path.join(__dirname, 'public')))
    .set('views', path.join(__dirname, 'views'))
    .set('view engine', 'ejs')
+  //  .use(session({secret: 'dont be suspicious', resave: 'false', saveUninitialized: 'true', cookie: { secure: true}}))
    .use(bodyParser.urlencoded({ extended: false }))
    .use('/', routes)
-   .use((req, res, next) => {
-    User.findById('5baa2528563f16379fc8a610')
-      .then(user => {
-        req.user = new User(user.name, user.email, user.cart, user._id);
-        next();
-      })
-      .catch(err => console.log(err));
+   .use(session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
   })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+)
+.use(csrfProtection)
+.use(flash())
 
-  //  mongoConnect(client => {
-  //  })
+.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+  .then(user => {
+    req.user = user;
+    next();
+  })
+  .catch(err => console.log(err));
+})
+
+.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+})
+  // .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+  mongoose
+  .connect(MONGODB_URI)
+  .then(result => {
+    // app.listen(5000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+  
    
 
    
