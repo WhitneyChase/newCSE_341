3a72135d477a7ed7bfd6829a3918aec1e2d39df1
WhitneyChase <64234289+WhitneyChase@users.noreply.github.com>
2020-10-17 22:43:57 -0600

    Create taIndex.js

---
+++ routes/ta/taIndex.js
@@ -0,0 +1,21 @@
+const routes = require('express').Router();
+const ta01 = require('./ta01');
+const ta02 = require('./ta02');
+const ta03 = require('./ta03'); 
+const ta04 = require('./ta04');
+const ta05 = require('./ta05'); 
+
+routes
+    .use('/ta01', ta01)
+    .use('/ta02', ta02)
+    .use('/ta03', ta03)
+    .use('/ta04', ta04)
+    .use('/ta05', ta05)
+    .get('/', (req, res, next) => {
+        res.render('pages/index', {
+            pageTitle: 'Team Activities', 
+            path: '/teamActivites'
+        })
+    });
+
+    module.exports = routes;
